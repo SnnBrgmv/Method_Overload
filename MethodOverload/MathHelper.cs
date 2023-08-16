6cdbec33ac679887f9ce8e8cddc6f7e99b580588
SnnBrgmv <sananai@code.edu.az>
2023-08-16 15:54:15 +0400

    C# Method Overload

---
+++ MethodOverload/MathHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    internal class MathHelper
+    {
+        public int PrintValue(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
+
+        public int PrintValue(bool value) 
+        {
+            if (value) return 1;
+            return 0;
+        }
+
+        public void PrintValue(string word1, string word2) 
+        {
+            Console.WriteLine($"{word1} {word2}");
+        }
+
+        public int PrintValue(int num1,int num2,int num3) 
+        {
+            return num1 * num2 * num3;
+        }
+    }
+}
---
+++ MethodOverload/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MethodOverload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathHelper mathHelper = new MathHelper();
+            Console.WriteLine(mathHelper.PrintValue(21,27));
+            Console.WriteLine(mathHelper.PrintValue(false));
+            mathHelper.PrintValue("Hello","P335");   
+            Console.WriteLine(mathHelper.PrintValue(3,5,6));
+        }
+    }
+}
